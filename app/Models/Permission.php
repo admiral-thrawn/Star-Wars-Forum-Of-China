5290ed09ae89ac7a29d52958cc7d73d1eb37ffeb
admiral-thrawn <2791794651@qq.com>
2020-12-12 10:52:55 +0800

    更新模型：增加角色和权限

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
+use App\Traits\UseUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * 权限模型
+ * @author admiral-thrawn
+ */
 class Permission extends Model
 {
-    use HasFactory;
+    use HasFactory, UseUuid, SoftDeletes;
+
+    /**
+     * 可以修改的字段
+     *
+     * @var array
+     */
+    protected $fillable = [
+        // 权限名称
+        'name',
+        // 介绍
+        'decription',
+    ];
+
+    /**
+     * 有此权限的角色
+     *
+     * @param string relationship role_permission
+     * @param string foreign_key permission_id
+     * @param string foreign_key role_id
+     *
+     * @return Role
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission', 'permission_id', 'role_id');
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,56 @@
 
 namespace App\Models;
 
+use App\Traits\UseUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * 角色模型
+ * @author admiral-thrawn
+ */
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, UseUuid, SoftDeletes;
+
+    /**
+     * 可以修改的字段
+     *
+     * @var array
+     */
+    protected $fillable = [
+        // 角色名称
+        'name',
+        // 介绍
+        'decription',
+    ];
+
+    /**
+     * 有此角色的用户
+     *
+     * @param string relationship user_role
+     * @param string foreign_key role_id
+     * @param string foreign_key user_id
+     *
+     * @return User
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_role', 'role_id', 'user_id');
+    }
+
+    /**
+     * 此角色的权限
+     *
+     * @param string relationship role_permission
+     * @param string foreign_key role_id
+     * @param string foreign_key permission_id
+     *
+     * @return Permission
+     */
+    public function perms()
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission', 'role_id', 'permission_id');
+    }
 }

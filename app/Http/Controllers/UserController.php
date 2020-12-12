8c84b4eb64008821b50a5398683df3cff5b80bc1
admiral-thrawn <2791794651@qq.com>
2020-12-12 15:57:46 +0800

    更新路由配置

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * 用户控制器
+ * @author admiral-thrawn
+ */
+class UserController extends Controller
+{
+    public function create()
+    {
+    }
+
+    public function find()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}

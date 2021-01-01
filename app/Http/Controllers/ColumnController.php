82e6403265c85eb60a2fb83385fbf6523948890a
admiral-thrawn <2791794651@qq.com>
2021-01-02 00:07:50 +0800

    专栏控制器

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Column;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ColumnController extends Controller
 {
-    //
+    public function index()
+    {
+        $columns = Column::pagenate(20);
+
+        return response([
+            'data' => $columns
+        ], Response::HTTP_OK);
+    }
+
+    public function show(Column $column)
+    {
+        return response([
+            'data' => $column
+        ], Response::HTTP_OK);
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function destroy()
+    {
+    }
 }

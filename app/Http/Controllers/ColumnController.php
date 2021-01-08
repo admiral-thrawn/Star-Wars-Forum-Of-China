7fa788a49c615953a92dbd7d1a2e6056323edc6f
admiral-thrawn <2791794651@qq.com>
2021-01-08 23:35:51 +0800

    store column

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Columns\StoreColumnRequest;
 use App\Http\Requests\Columns\UpdateColumnRequest;
 use App\Models\Column;
 use Illuminate\Http\Response;
+use Silber\Bouncer\BouncerFacade as Bouncer;
 
 class ColumnController extends Controller
 {
@@ -27,6 +28,21 @@ class ColumnController extends Controller
 
     public function store(StoreColumnRequest $request)
     {
+        $validatedData = $request->validate();
+
+        $column = new Column($validatedData);
+
+        $user = $request->user();
+
+        $column->save();
+
+        // 授权用户拥有此专栏
+        Bouncer::allow($user)->toOwn($column)->to(['view', 'update', 'delete']);
+
+        // 返回专栏和200状态码
+        return response([
+            'data' => $column
+        ], Response::HTTP_OK);
     }
 
     public function update(UpdateColumnRequest $request)

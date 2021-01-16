b7df90b55a83ba3deff6c1e0fec86ad19209ca10
admiral-thrawn <2791794651@qq.com>
2021-01-16 19:17:47 +0800

    文章和用户测试

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,8 +28,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'description' => $this->faker->sentence(10),
-            'slogan' => $this->faker->text()
+            'description' => $this->faker->text(),
+            'slogan' => $this->faker->sentence(5),
+            'avatar' => 'default.jpg'
         ];
     }
 }
